8b32fc8f7aae3d3f3c1759757dbc391a85de91db
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-17 13:44:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "23990433"))
+    API_HASH = getenv("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5993120667:AAGJpWmuj9gSByI9tpW9gQMfBtJblYzFZ4Y")
+    FSUB = getenv("FSUB", "SK_HD_MOVIE")
+    CHID = int(getenv("CHID", "-1001915530102"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
